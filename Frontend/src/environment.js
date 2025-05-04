876fb9330305a4a8bffe2e054885b6f76564f7eb
“[firstname <yadavamit8968@gmail.com>
2025-05-04 09:09:00 +0530

    Server Done

--- Frontend/src/environment.js
+++ Frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD
   ? "http://localhost:5001"
-  : "https://blog-world-3son.onrender.com/";
+  : "https://blog-world-3son.onrender.com";
 
 export default server;
